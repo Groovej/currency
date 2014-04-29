23d320016ca822effd024fd4e76f3e3276b2e519
Bogdan <groovejbogdan@yandex.ru>
2014-04-29 18:22:33 +0300

    production changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,8 @@ Currency::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  #config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(mangle: false)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
@@ -77,4 +78,5 @@ Currency::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
 end
