b5b65d93c93188a21c3409fabae125da114de744
Naumenko Bogdan <groovejbogdan@yandex.ru>
2016-03-06 20:59:24 +0200

    remove validation

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,8 +1,6 @@
 class Country < ActiveRecord::Base
   has_many :currency_types
 
-	validates_uniqueness_of :name
-
   scope :visited, -> { where(visited: true) }
   scope :remaining, -> { where(visited: false) }
 end
